a957a7c88248b7d6e46feef870ae7aebf17af2b2
c <c>
2017-08-10 19:48:57 +1000

    Reduced HC speed to 1

--- config.lua
+++ config.lua
@@ -8,8 +8,8 @@ Config = {
 	MachineCraftingSpeed = 5,
 	
 	--Desc: Multiplier for manual crafting speed (by hand)
-	--Default: 5 (Items are created 5 times as fast)
-	HandCraftingSpeed = 5,
+	--Default: 1 (Items are created 5 times as fast)
+	HandCraftingSpeed = 1,
 	
 	--Desc: Multiplier for research speed
 	--Default: 5 (Research is completed 5 times as fast)
